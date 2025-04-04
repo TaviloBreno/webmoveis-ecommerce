bcfc37213b487c67dd0f7fc70f541c9cd724a515
Tavilo Breno <breno_wk2@hotmail.com>
2025-04-04 12:39:25 -0300

    feat: initial config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('WebMoveis API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentationConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
