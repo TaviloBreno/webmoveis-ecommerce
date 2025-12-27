72c22bc2b6f83ba9d82b8f17a956f0eb0664071a
Tavilo Breno <breno_wk2@hotmail.com>
2025-12-26 23:17:54 -0300

    chore: add testing scripts and dependencies for Jest and Testing Library

---
+++ frontend/jest.setup.ts
@@ -0,0 +1,45 @@
+import '@testing-library/jest-dom';
+
+// Mock do next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+    };
+  },
+  useSearchParams() {
+    return {
+      get: jest.fn(),
+    };
+  },
+  usePathname() {
+    return '';
+  },
+}));
+
+// Mock do next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />;
+  },
+}));
+
+// Mock do framer-motion
+jest.mock('framer-motion', () => ({
+  motion: {
+    div: ({ children, ...props }: any) => <div {...props}>{children}</div>,
+    button: ({ children, ...props }: any) => <button {...props}>{children}</button>,
+    a: ({ children, ...props }: any) => <a {...props}>{children}</a>,
+    span: ({ children, ...props }: any) => <span {...props}>{children}</span>,
+  },
+  AnimatePresence: ({ children }: any) => <>{children}</>,
+  useScroll: () => ({
+    scrollYProgress: { get: () => 0 },
+  }),
+  useTransform: () => 0,
+}));
